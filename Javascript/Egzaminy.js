264af8536902b8b0e5df4c25ef5047092aa1b581
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-01-02 18:39:07 +0100

    nowe js

---
+++ Javascript/Egzaminy.js
@@ -0,0 +1,3 @@
+document.getElementById('contentcontainer').style.height = "2100px";
+document.getElementById('podstrona').style.height = "2050px";
+document.getElementById('podstrona').style.width = "96%";

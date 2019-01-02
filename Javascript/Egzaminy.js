96b76370f539fb362fc9cd14de27a648603f6222
belbekyt <45708521+belbekyt@users.noreply.github.com>
2019-01-02 21:07:09 +0100

    Update Egzaminy.js
    mała poprawka rozmiarów

--- Javascript/Egzaminy.js
+++ Javascript/Egzaminy.js
@@ -1,3 +1,4 @@
-document.getElementById('contentcontainer').style.height = "2100px";
+document.getElementById('contentcontainer').style.height = "2080px";
 document.getElementById('podstrona').style.height = "2050px";
 document.getElementById('podstrona').style.width = "96%";
+document.getElementById('menuright').style.height = "2080px";

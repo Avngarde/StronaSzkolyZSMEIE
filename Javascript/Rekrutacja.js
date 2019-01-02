1a50857cf6561c21173f7199b590054a1f046a8e
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-01-02 18:16:32 +0100

    nowe js do podstronek

---
+++ Javascript/Rekrutacja.js
@@ -0,0 +1,3 @@
+document.getElementById('contentcontainer').style.height = "1500px";
+document.getElementById('podstrona').style.height = "1400px";
+document.getElementById('podstrona').style.width = "96%";
---
+++ Javascript/Terminy.js
@@ -0,0 +1,2 @@
+document.getElementById('contentcontainer').style.height = "2700px";
+document.getElementById('podstrona').style.height = "2600px";

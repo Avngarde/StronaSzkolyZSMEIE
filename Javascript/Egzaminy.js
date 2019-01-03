b2ea429e8e75972e7962f5026846326e9df5f040
Zeusiek <zeusiek@op.pl>
2019-01-03 01:25:59 +0100

    Delete Egzaminy.js

--- Javascript/Egzaminy.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contentcontainer').style.height = "2080px";
-document.getElementById('podstrona').style.height = "2050px";
-document.getElementById('podstrona').style.width = "96%";
-document.getElementById('menuright').style.height = "2080px";

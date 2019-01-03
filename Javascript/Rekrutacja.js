cd72c6e9cd3065548cf8b061c0ba985c58c3a0f2
Zeusiek <zeusiek@op.pl>
2019-01-03 01:26:37 +0100

    Delete Rekrutacja.js

--- Javascript/Rekrutacja.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contentcontainer').style.height = "2000px";
-document.getElementById('podstrona').style.height = "1970px";
-document.getElementById('podstrona').style.width = "96%";
-document.getElementById('menuright').style.height = "2000px";

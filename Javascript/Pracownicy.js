5d718af05ee72dd09eee16f2fba2a73a9f1fdcb2
Zeusiek <zeusiek@op.pl>
2019-01-03 01:26:20 +0100

    Delete Pracownicy.js

--- Javascript/Pracownicy.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('contentcontainer').style.height = "2450px";
-document .getElementById('podstrona').style.height = "2420px";
-document.getElementById('menuright').style.height = "2450px";

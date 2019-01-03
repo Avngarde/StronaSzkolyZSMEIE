ede9052deac8986197ae18b8140a2a6281ac9bd2
Zeusiek <zeusiek@op.pl>
2019-01-03 01:26:44 +0100

    Delete Terminy.js

--- Javascript/Terminy.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('contentcontainer').style.height = "2700px";
-document.getElementById('podstrona').style.height = "2670px";
-document.getElementById('menuright').style.height = "2700px";

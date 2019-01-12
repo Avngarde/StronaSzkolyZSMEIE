7b578b4a5f301edd89e2f11e16b10891944c4838
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-01-12 20:00:14 +0100

    js do artykułów

--- javascript/artykuly1.js
+++ javascript/artykuly1.js
@@ -1,25 +1,64 @@
 var y = 0;
-var i = 0;
+var i;
+var a;
 
 
-var mniej = document.getElementsByClassName('mniej')[0];
-  mniej.style.visibility = "hidden";
-var text = document.getElementsByClassName('rozwiniecie')[0];
-  text.style.visibility = "hidden";
+var mniej = document.getElementsByClassName('mniej');
+var text = document.getElementsByClassName('rozwiniecie');
 
-
-function expand()
+for(i=0; i<5; i+=1)
 {
-  document.getElementsByClassName('articles')[y].style.height = "320px";
-  document.getElementsByClassName('wiecej')[y].style.visibility = "hidden";
-  mniej.style.visibility = "visible";
-  text.style.visibility = "visible";
+  mniej[i].style.visibility = "hidden";
+  text[i].style.visibility = "hidden";
 }
 
-function fold()
+
+function expand(a)
 {
-  document.getElementsByClassName('articles')[y].style.height = "130px";
-  document.getElementsByClassName('wiecej')[y].style.visibility = "visible";
-  mniej.style.visibility = "hidden";
-  text.style.visibility = "hidden";
+  if(a=='0')
+  {
+    document.getElementsByClassName('articles')[0].style.height = "320px";
+    document.getElementsByClassName('wiecej')[0].style.visibility = "hidden";
+    mniej[0].style.visibility = "visible";
+    text[0].style.visibility = "visible";
+  }
+  if(a=='1')
+  {
+    document.getElementsByClassName('articles')[1].style.height = "320px";
+    document.getElementsByClassName('wiecej')[1].style.visibility = "hidden";
+    mniej[1].style.visibility = "visible";
+    text[1].style.visibility = "visible";
+  }
+  if(a=='2')
+  {
+    document.getElementsByClassName('articles')[2].style.height = "320px";
+    document.getElementsByClassName('wiecej')[2].style.visibility = "hidden";
+    mniej[2].style.visibility = "visible";
+    text[2].style.visibility = "visible";
+  }
+}
+
+function fold(a)
+{
+  if(a=='0')
+  {
+    document.getElementsByClassName('articles')[0].style.height = "130px";
+    document.getElementsByClassName('wiecej')[0].style.visibility = "visible";
+    mniej[0].style.visibility = "hidden";
+    text[0].style.visibility = "hidden";
+  }
+  if(a=='1')
+  {
+    document.getElementsByClassName('articles')[1].style.height = "130px";
+    document.getElementsByClassName('wiecej')[1].style.visibility = "visible";
+    mniej[1].style.visibility = "hidden";
+    text[1].style.visibility = "hidden";
+  }
+  if(a=='2')
+  {
+    document.getElementsByClassName('articles')[2].style.height = "130px";
+    document.getElementsByClassName('wiecej')[2].style.visibility = "visible";
+    mniej[2].style.visibility = "hidden";
+    text[2].style.visibility = "hidden";
+  }
 }

5cf62b305302a58b3976414dbc8a2dff8a32dd2d
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-01-15 14:01:36 +0100

    działajace "inteligentne zwijanie" artykułów

--- javascript/artykuly1.js
+++ javascript/artykuly1.js
@@ -48,7 +48,55 @@ function expand(a)
   }
   else if(rozwiniecie == true)
   {
-    alert("kjasdh");
+    if(previous == 0)
+    {
+      document.getElementsByClassName('articles')[0].style.height = "130px";
+      document.getElementsByClassName('wiecej')[0].style.visibility = "visible";
+      mniej[0].style.visibility = "hidden";
+      text[0].style.visibility = "hidden";
+    }
+    if(previous == 1)
+    {
+      document.getElementsByClassName('articles')[1].style.height = "130px";
+      document.getElementsByClassName('wiecej')[1].style.visibility = "visible";
+      mniej[1].style.visibility = "hidden";
+      text[1].style.visibility = "hidden";
+    }
+    if(previous == 2)
+    {
+      document.getElementsByClassName('articles')[2].style.height = "130px";
+      document.getElementsByClassName('wiecej')[2].style.visibility = "visible";
+      mniej[2].style.visibility = "hidden";
+      text[2].style.visibility = "hidden";
+    }
+    if(a=='0')
+    {
+      document.getElementsByClassName('articles')[0].style.height = "320px";
+      document.getElementsByClassName('wiecej')[0].style.visibility = "hidden";
+      mniej[0].style.visibility = "visible";
+      text[0].style.visibility = "visible";
+      previous = 0;
+      rozwiniecie = true;
+    }
+    if(a=='1')
+    {
+     document.getElementsByClassName('articles')[1].style.height = "320px";
+     document.getElementsByClassName('wiecej')[1].style.visibility = "hidden";
+     mniej[1].style.visibility = "visible";
+     text[1].style.visibility = "visible";
+     previous = 1;
+     rozwiniecie = true;
+    }
+    if(a=='2')
+    {
+      document.getElementsByClassName('articles')[2].style.height = "320px";
+      document.getElementsByClassName('wiecej')[2].style.visibility = "hidden";
+      mniej[2].style.visibility = "visible";
+      text[2].style.visibility = "visible";
+      previous = 2;
+      rozwiniecie = true;
+    }
+
   }
     
 }
@@ -61,6 +109,7 @@ function fold(a)
     document.getElementsByClassName('wiecej')[0].style.visibility = "visible";
     mniej[0].style.visibility = "hidden";
     text[0].style.visibility = "hidden";
+    rozwiniecie
   }
   if(a=='1')
   {
@@ -77,4 +126,3 @@ function fold(a)
     text[2].style.visibility = "hidden";
   }
 }
-p

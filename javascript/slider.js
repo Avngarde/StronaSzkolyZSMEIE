af4669118e17a47ba43a8b4ddba70a9d36901e30
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-01-31 21:53:05 +0100

    dokończony slider

--- javascript/slider.js
+++ javascript/slider.js
@@ -2,6 +2,7 @@ var a;
 var b = 0;
 var previous = 0;
 var c;
+var d = 0;
 
 $('.prev').eq(0).mouseover(function () { 
    $('.prev > a').eq(0).css('color', 'white'); 
@@ -19,16 +20,15 @@ $('.prev').eq(1).mouseout(function () {
     $('.prev > a').eq(1).css('color', 'black');
 });
 
-
 document.getElementsByClassName('slide')[0].style.display = 'block';
 
 function plusSlides(a)
 {
     
-    if(b == 3)
+    if(b == 5)
     {
         b=-1;
-        document.getElementsByClassName('slide')[3].style.display = "none";
+        document.getElementsByClassName('slide')[5].style.display = "none";
         document.getElementsByClassName('slide')[b+=a].style.display = "block"; 
     }
     else
@@ -43,9 +43,9 @@ function minusSlides(a)
 {
     if(b == 0)
     {
-        b=3;
+        b=5;
         document.getElementsByClassName('slide')[0].style.display = "none";
-        document.getElementsByClassName('slide')[3].style.display = "block";
+        document.getElementsByClassName('slide')[5].style.display = "block";
     }
     else
     {

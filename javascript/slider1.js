8f1be3358e3fb318d18a6cc6ced6f8cace124231
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-02-17 20:20:43 +0100

    zaktualizowane skrypty do slidera w rekrutacji

--- javascript/slider1.js
+++ javascript/slider1.js
@@ -4,39 +4,29 @@ var previous = 0;
 var c;
 var d = 0;
 
-$('.prev').eq(0).mouseover(function () { 
-   $('.prev > a').eq(0).css('color', 'white'); 
-});
-
-$('.prev').eq(1).mouseover(function () { 
-    $('.prev > a').eq(1).css('color', 'white'); 
- });
-
-$('.prev').eq(0).mouseout(function () { 
-    $('.prev > a').eq(0).css('color', 'black');
-});
-
-$('.prev').eq(1).mouseout(function () { 
-    $('.prev > a').eq(1).css('color', 'black');
-});
-
  document.getElementsByClassName('slide')[0].style.display = 'block';
+ document.getElementsByClassName('arrow')[0].style.display = "block";
+ document.getElementsByClassName('arrow')[1].style.display = "block";
 
  function plusSlides(a)
 {
-    
+
     if(b == 7)
     {
         b=-1;
         document.getElementsByClassName('slide')[7].style.display = "none";
-        document.getElementsByClassName('slide')[b+=a].style.display = "block"; 
+        document.getElementsByClassName('slide')[b+=a].style.display = "block";
+        document.getElementsByClassName('arrow')[0].style.display = "block";
+        document.getElementsByClassName('arrow')[1].style.display = "block";
     }
     else
     {
         document.getElementsByClassName('slide')[b].style.display = "none";
-        document.getElementsByClassName('slide')[b+=a].style.display = "block"; 
+        document.getElementsByClassName('slide')[b+=a].style.display = "block";
+        document.getElementsByClassName('arrow')[0].style.display = "block";
+        document.getElementsByClassName('arrow')[1].style.display = "block";
     }
-         
+
 }
 
 function minusSlides(a)
@@ -46,11 +36,15 @@ function minusSlides(a)
         b=7;
         document.getElementsByClassName('slide')[0].style.display = "none";
         document.getElementsByClassName('slide')[7].style.display = "block";
+        document.getElementsByClassName('arrow')[0].style.display = "block";
+        document.getElementsByClassName('arrow')[1].style.display = "block";
     }
     else
     {
         document.getElementsByClassName('slide')[b].style.display = "none";
         document.getElementsByClassName('slide')[b-=a].style.display = "block";
+        document.getElementsByClassName('arrow')[0].style.display = "block";
+        document.getElementsByClassName('arrow')[1].style.display = "block";
     }
-    
+
 }

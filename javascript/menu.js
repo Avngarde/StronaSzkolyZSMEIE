8c3e660594d039a59dd008fbe9342e64829fdf2f
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-02-28 19:22:32 +0100

    po zmianach matiego na komputerach wszystko się dobrze rozwija

--- javascript/menu.js
+++ javascript/menu.js
@@ -80,6 +80,7 @@ else
         $('.menuoption').eq(9).css('display', 'block');
         $('.menuoption').eq(10).css('display', 'block');
         $('.menuoption').eq(11).css('display', 'block');
+        $('.menuoption').eq(12).css('display', 'block');
     });
     document.getElementsByClassName('menubuttons')[2].addEventListener("mouseout", function(){
         $('.optionholder').eq(1).css('display', 'none');  
@@ -88,27 +89,38 @@ else
     
     document.getElementsByClassName("menubuttons")[3].addEventListener("mouseover", function(){
         $('.optionholder').eq(2).css('display', 'block');
-        $('.menuoption').eq(12).css('display', 'block');
-        $('.menuoption').eq(13).css('display', 'block');
-       
-    
-       
-    
+        $('.menuoption').eq(13).css('display', 'block'); 
     });
     document.getElementsByClassName('menubuttons')[3].addEventListener("mouseout", function(){
         $('.optionholder').eq(2).css('display', 'none');  
     });
-    
-    document.getElementsByClassName("menubuttons")[6].addEventListener("mouseover", function(){
+
+    document.getElementsByClassName('menubuttons')[4].addEventListener("mouseover", function(){
         $('.optionholder').eq(3).css('display', 'block');
         $('.menuoption').eq(14).css('display', 'block');
         $('.menuoption').eq(15).css('display', 'block');
+    });
+    document.getElementsByClassName('menubuttons')[4].addEventListener("mouseout", function(){
+        $('.optionholder').eq(3).css('display', 'none');
+    });
+
+    document.getElementsByClassName('menubuttons')[5].addEventListener("mouseover", function(){
+        $('.optionholder').eq(4).css('display', 'block');
+        $('.menuoption').eq(15).css('display', 'block');
+    });
+    document.getElementsByClassName('menubuttons')[5].addEventListener("mouseout", function(){
+        $('.optionholder').eq(4).css('display', 'none');
+    });
+
+    document.getElementsByClassName("menubuttons")[6].addEventListener("mouseover", function(){
+        $('.optionholder').eq(5).css('display', 'block');  
         $('.menuoption').eq(16).css('display', 'block');
         $('.menuoption').eq(17).css('display', 'block');
-    
+        $('.menuoption').eq(18).css('display', 'block');
+        $('.menuoption').eq(19).css('display', 'block');  
     });
     document.getElementsByClassName('menubuttons')[6].addEventListener("mouseout", function(){
-        $('.optionholder').eq(3).css('display', 'none');  
+        $('.optionholder').eq(5).css('display', 'none');  
     });
     
     

20a529159e51a1a052ed353013f1636c0713409c
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-03-30 16:37:07 +0100

    rozszerzenie dla 10 artykułów

--- javascript/animacje.js
+++ javascript/animacje.js
@@ -4,6 +4,14 @@ var pozostaly_rozwiniety = false
 var rozwiniecie0 = false;
 var rozwiniecie1 = false;
 var rozwiniecie2 = false;
+var rozwiniecie3 = false;
+var rozwiniecie4 = false;
+var rozwiniecie5 = false;
+var rozwiniecie6 = false;
+var rozwiniecie7 = false;
+var rozwiniecie8 = false;
+var rozwiniecie9 = false;
+var rozwiniecie10 = false;
 var button = false;
 var previous;
 var mniej = "Mniej...";
@@ -19,10 +27,12 @@ if(warunek1 > 700)
    if(wysokosc1 >= wysokosc2)
    {
       $('.themepages').eq(1).height(wysokosc1);
+      $('#menuright').height(wysokosc1 * 1.04);
    }
-   else
+   else if(wysokosc1 < wysokosc2)
    {
       $('.themepages').eq(0).height(wysokosc2);
+      $('#menuright').height(wysokosc2 * 1.04);
    }
 
 
@@ -61,7 +71,7 @@ document.getElementById('rightmenubutton').addEventListener("click", function(){
    
 });
 
-//.................................................
+//........................................................................................
 
 $('.rozwiniecie').hide();
 
@@ -71,7 +81,7 @@ document.getElementsByClassName("button")[0].addEventListener("click", function(
    {
       expand(0); 
    }
-   else if(unek1rozwiniecie0 == true)
+   else if(rozwiniecie0 == true)
    {
       fold(0);
    }       
@@ -101,6 +111,103 @@ document.getElementsByClassName("button")[2].addEventListener("click", function(
    }       
 });
 
+document.getElementsByClassName("button")[3].addEventListener("click", function()
+{
+   if(rozwiniecie3 == false)
+   {
+      expand(3); 
+   }
+   else if(rozwiniecie3 == true)
+   {
+      fold(3);
+   }       
+});
+
+document.getElementsByClassName("button")[4].addEventListener("click", function()
+{
+   if(rozwiniecie4 == false)
+   {
+      expand(4); 
+   }
+   else if(rozwiniecie4 == true)
+   {
+      fold(4);
+   }       
+});
+
+document.getElementsByClassName("button")[5].addEventListener("click", function()
+{
+   if(rozwiniecie5 == false)
+   {
+      expand(5); 
+   }
+   else if(rozwiniecie5 == true)
+   {
+      fold(5);
+   }       
+});
+
+document.getElementsByClassName("button")[6].addEventListener("click", function()
+{
+   if(rozwiniecie6 == false)
+   {
+      expand(6); 
+   }
+   else if(rozwiniecie6 == true)
+   {
+      fold(6);
+   }       
+});
+
+document.getElementsByClassName("button")[7].addEventListener("click", function()
+{
+   if(rozwiniecie7 == false)
+   {
+      expand(7); 
+   }
+   else if(rozwiniecie7 == true)
+   {
+      fold(7);
+   }       
+});
+
+document.getElementsByClassName("button")[8].addEventListener("click", function()
+{
+   if(rozwiniecie8 == false)
+   {
+      expand(8); 
+   }
+   else if(rozwiniecie8 == true)
+   {
+      fold(8);
+   }       
+});
+
+document.getElementsByClassName("button")[9].addEventListener("click", function()
+{
+   if(rozwiniecie9 == false)
+   {
+      expand(9); 
+   }
+   else if(rozwiniecie9 == true)
+   {
+      fold(9);
+   }       
+});
+
+document.getElementsByClassName("button")[2].addEventListener("click", function()
+{
+   if(rozwiniecie10 == false)
+   {
+      expand(10); 
+   }
+   else if(rozwiniecie10 == true)
+   {
+      fold(10);
+   }       
+});
+
+
 
 function expand(indeks1)
 {
@@ -115,6 +222,14 @@ function expand(indeks1)
              rozwiniecie0 = false;
              rozwiniecie1 = false;
              rozwiniecie2 = false;
+             rozwiniecie3 = false;
+             rozwiniecie4 = false;
+             rozwiniecie5 = false;
+             rozwiniecie6 = false;
+             rozwiniecie7 = false;
+             rozwiniecie8 = false;
+             rozwiniecie9 = false;
+             rozwiniecie10 = false;
              if(indeks1 == 0)
              {
                 rozwiniecie0 = true;
@@ -126,6 +241,38 @@ function expand(indeks1)
              else if(indeks1 == 2)
              {
                 rozwiniecie2 = true;
+             }
+             else if(indeks1 == 3)
+             {
+                rozwiniecie3 = true;
+             }
+             else if(indeks1 == 4)
+             {
+                rozwiniecie4 = true;
+             }
+             else if(indeks1 == 5)
+             {
+                rozwiniecie5 = true;
+             }
+             else if(indeks1 == 6)
+             {
+                rozwiniecie6 = true;
+             }
+             else if(indeks1 == 7)
+             {
+                rozwiniecie7 = true;
+             }
+             else if(indeks1 == 8)
+             {
+                rozwiniecie8 = true;
+             }
+             else if(indeks1 == 9)
+             {
+                rozwiniecie9 = true;
+             }
+             else if(indeks1 == 10)
+             {
+                rozwiniecie10 = true;
              }
              previous = indeks1;
              pozostaly_rozwiniety = true;
@@ -152,6 +299,14 @@ function expand(indeks1)
                    rozwiniecie0 = false;
                    rozwiniecie1 = false;
                    rozwiniecie2 = false;
+                   rozwiniecie3 = false;
+                   rozwiniecie4 = false;
+                   rozwiniecie5 = false;
+                   rozwiniecie6 = false;
+                   rozwiniecie7 = false;
+                   rozwiniecie8 = false;
+                   rozwiniecie9 = false;
+                   rozwiniecie10 = false;
                    if(indeks1 == 0)
                    {
                       rozwiniecie0 = true;
@@ -163,6 +318,38 @@ function expand(indeks1)
                    else if(indeks1 == 2)
                    {
                       rozwiniecie2 = true;
+                   }
+                   else if(indeks1 == 3)
+                   {
+                      rozwiniecie3 = true;
+                   }
+                   else if(indeks1 == 4)
+                   {
+                      rozwiniecie4 = true;
+                   }
+                   else if(indeks1 == 5)
+                   {
+                      rozwiniecie5 = true;
+                   }
+                   else if(indeks1 == 6)
+                   {
+                      rozwiniecie6 = true;
+                   }
+                   else if(indeks1 == 7)
+                   {
+                      rozwiniecie7 = true;
+                   }
+                   else if(indeks1 == 8)
+                   {
+                      rozwiniecie8 = true;
+                   }
+                   else if(indeks1 == 9)
+                   {
+                      rozwiniecie9 = true;
+                   }
+                   else if(indeks1 == 10)
+                   {
+                      rozwiniecie10 = true;
                    }
                    previous = indeks1;
                    pozostaly_rozwiniety = true;
@@ -183,17 +370,49 @@ function fold(indeks2)
          $('.button > a').eq(indeks2).delay(4000);
          $('.button > a').eq(indeks2).html(wiecej);
          if(indeks2 == 0)
-             {
-                rozwiniecie0 = false;
-             }
-             else if(indeks2 == 1)
-             {
-                rozwiniecie1 = false;
-             }
-             else if(indeks2 == 2)
-             {
-                rozwiniecie2 = false;
-             }
+                   {
+                      rozwiniecie0 = false;
+                   }
+                   else if(indeks2 == 1)
+                   {
+                      rozwiniecie1 = false;
+                   }
+                   else if(indeks2 == 2)
+                   {
+                      rozwiniecie2 = false;
+                   }
+                   else if(indeks2 == 3)
+                   {
+                      rozwiniecie3 = false;
+                   }
+                   else if(indeks2 == 4)
+                   {
+                      rozwiniecie4 = false;
+                   }
+                   else if(indeks2 == 5)
+                   {
+                      rozwiniecie5 = false;
+                   }
+                   else if(indeks2 == 6)
+                   {
+                      rozwiniecie6 = false;
+                   }
+                   else if(indeks2 == 7)
+                   {
+                      rozwiniecie7 = false;
+                   }
+                   else if(indeks2 == 8)
+                   {
+                      rozwiniecie8 = false;
+                   }
+                   else if(indeks2 == 9)
+                   {
+                      rozwiniecie9 = false;
+                   }
+                   else if(indeks2 == 10)
+                   {
+                      rozwiniecie10 = false;
+                   }
       });    
    });
    pozostaly_rozwiniety = false;

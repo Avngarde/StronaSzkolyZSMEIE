1710b5dfc2d620191c31ff424649a263af890678
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2019-04-02 20:48:39 +0200

    Update settings.py

--- stronaszkoly/settings.py
+++ stronaszkoly/settings.py
@@ -70,13 +70,9 @@ WSGI_APPLICATION = 'stronaszkoly.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'schooldb',
-        'USER':'######',
-        'PASSWORD':'##########',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

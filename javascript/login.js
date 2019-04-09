c2c6de6e647f4d543e7dd6014b3b7ab5cb921abd
REVANPL <43894551+REVANPL@users.noreply.github.com>
2019-04-09 10:43:08 +0200

    js do loginu

---
+++ javascript/login.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('inputy')[0].addEventListener("click", function(){
+    document.getElementById('legend').style.color = '#668cff';
+    document.getElementsByClassName('fieldset')[0].style.borderColor = '#668cff';
+});
+
+

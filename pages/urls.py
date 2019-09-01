7b6734d6ecefdcdf50a1e9019f40be8143f2ebdd
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2019-09-01 19:45:41 +0200

    Add files via upload

--- pages/urls.py
+++ pages/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import handler404,handler500
 from . import views
 
 urlpatterns = [
